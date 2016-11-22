aec7c6961668963d2327ffae3c1cdd4b790337b9
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-11-22 20:50:24 +0100

    Added MenuItemBean

---
+++ src/database/MenuItemBean.java
@@ -0,0 +1,62 @@
+package database;
+
+public class MenuItemBean extends Bean {
+
+	private static final long serialVersionUID = 1L;
+	private static final String table = "menufinder.menuitem";
+
+	private long menu;
+	private long item;
+	private long itemCategory;
+
+	public MenuItemBean() {
+		super();
+		isNewRecord = true;
+	}
+
+	public MenuItemBean(long menu, long item, long itemCategory) {
+		super();
+		this.menu = menu;
+		this.item = item;
+		this.itemCategory = itemCategory;
+		isNewRecord = true;
+	}
+
+	public long getMenu() {
+		return menu;
+	}
+
+	public void setMenu(long menu) {
+		this.menu = menu;
+	}
+
+	public long getItem() {
+		return item;
+	}
+
+	public void setItem(long item) {
+		this.item = item;
+	}
+
+	public long getItemCategory() {
+		return itemCategory;
+	}
+
+	public void setItemCategory(long itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+
+	protected String getDeleteQuery() {
+		return "DELETE FROM " + table + " WHERE menu = " + menu + " item = " + item + " itemcategory = " + itemCategory;
+	}
+
+	protected String getUpdateQuery() {
+		return null;
+	}
+
+	protected String getInsertQuery() {
+		return "INSERT INTO " + table + " (menu, item, itemcategory) VALUES (" + menu + ", " + item + ", "
+				+ itemCategory + ")";
+	}
+
+}
