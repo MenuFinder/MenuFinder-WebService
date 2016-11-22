7ed86e58d80fcee087a3f2d423d09727157de159
Marc Sánchez <sanchezfauste@gmail.com>
2016-11-22 17:24:06 +0100

    Changing visibility of some methods to protected

--- src/database/DBManager.java
+++ src/database/DBManager.java
@@ -42,7 +42,7 @@ public class DBManager {
 		return (DataSource) new InitialContext().lookup(datasource);
 	}
 
-	public void deleteBean(Bean m) {
+	protected void deleteBean(Bean m) {
 		try {
 			DBManager db = DBManager.getInstance();
 			db.executeUpdate(m.getDeleteQuery());
@@ -52,7 +52,7 @@ public class DBManager {
 		}
 	}
 
-	public void updateBean(Bean m) {
+	protected void updateBean(Bean m) {
 		try {
 			DBManager db = DBManager.getInstance();
 			db.executeUpdate(m.getUpdateQuery());
@@ -62,7 +62,7 @@ public class DBManager {
 		}
 	}
 
-	public void insertBean(Bean m) {
+	protected void insertBean(Bean m) {
 		try {
 			DBManager db = DBManager.getInstance();
 			db.executeUpdate(m.getInsertQuery());
