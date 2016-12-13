37f1702f68bf56f1910d077d995b860a54704f8f
Marc Sánchez <sanchezfauste@gmail.com>
2016-12-13 17:23:04 +0100

    Adding AccountSubscriptionBean

---
+++ src/database/AccountSubscriptionBean.java
@@ -0,0 +1,58 @@
+package database;
+
+public class AccountSubscriptionBean extends Bean {
+
+	private static final long serialVersionUID = 1L;
+	private static final String table = "usersubscription";
+
+	private String account;
+	private long restaurant;
+
+	public AccountSubscriptionBean() {
+		super();
+		isNewRecord = true;
+	}
+
+	public AccountSubscriptionBean(String account, long restaurant) {
+		super();
+		this.account = account;
+		this.restaurant = restaurant;
+		isNewRecord = true;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public long getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(long restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public static String getTable() {
+		return table;
+	}
+
+	@Override
+	protected String getDeleteQuery() {
+		return "DELETE FROM " + table + " WHERE account = '" + account + "' AND restaurant = " + restaurant;
+	}
+
+	@Override
+	protected String getUpdateQuery() {
+		return null;
+	}
+
+	@Override
+	protected String getInsertQuery() {
+		return "INSERT INTO " + table + " (account, restaurant) VALUES ('" + account + "', " + restaurant + ")";
+	}
+
+}
